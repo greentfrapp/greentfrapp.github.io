e5247aebc337701fd041fef8f64933372ae2d18d
SK <limsweekiat@gmail.com>
2022-02-17 22:29:38 +0800

    Send message

--- assets/test_localstorage_set.js
+++ assets/test_localstorage_set.js
@@ -1,6 +1,14 @@
-console.log('test set')
-const person = {
-  name: "Mirio Togawa",
-  location: "BHA",
-}
-window.localStorage.setItem('user', JSON.stringify(person))
+// console.log('test set')
+// const person = {
+//   name: "Mirio Togawa",
+//   location: "BHA",
+// }
+// window.localStorage.setItem('user', JSON.stringify(person))
+
+const iframe = document.getElementById('messengerIframe')
+iframe.contentWindow.postMessage({
+  action: 'save',
+  key: 'key',
+  value: 'hello world'
+})
+console.log('value set')
