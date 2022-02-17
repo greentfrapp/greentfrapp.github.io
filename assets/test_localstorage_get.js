e5e1b98ab0ba47f82521cb55685d8218ffadaa4c
SK <limsweekiat@gmail.com>
2022-02-18 01:01:29 +0800

    Remove localStorage js files

--- assets/test_localstorage_get.js
+++
@@ -1,2 +0,0 @@
-console.log('test get')
-console.log(JSON.parse(window.localStorage.getItem('user')))
--- assets/test_localstorage_msg.js
+++
@@ -1,6 +0,0 @@
-console.log("msg receiver loaded")
-console.log(localStorage.getItem('user'))
-window.onmessage = function(e) {
-    console.log('got message')
-    console.log(localStorage.getItem('user'))
-}
