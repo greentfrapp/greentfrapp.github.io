4095587188d5bdb4598983017fcdb9fb5abe1a0b
SK <limsweekiat@gmail.com>
2022-02-17 22:00:23 +0800

    Use message receiver to get localStorage

--- assets/test_localstorage_msg.js
+++ assets/test_localstorage_msg.js
@@ -1,4 +1,6 @@
 console.log("msg receiver loaded")
+console.log(localStorage.getItem('user'))
 window.onmessage = function(e) {
     console.log('got message')
+    console.log(localStorage.getItem('user'))
 }
